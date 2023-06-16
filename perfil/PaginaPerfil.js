a8afdd264d8b5616eedd16384fd25bd6c03e6696
Matheus Melgaço <matheusmelgacob@gmail.com>
2023-06-16 00:30:54 -0300

    Navbar adaptada para todas as resoluções

--- perfil/PaginaPerfil.js
+++ perfil/PaginaPerfil.js
@@ -36,33 +36,35 @@ const initLogin = function () {
 
     // Mostra a navbar para quando o usuário não está logado
     document.getElementById('navbarLoggedOut').innerHTML = `
-          <nav class="navbar navbar-expand-lg bg-body-tertiary" style=" height: 73px; background-color: #fff">
-          <div class="container-fluid" style="padding-left: 160px; ">
-            <a class="navbar-brand" href="#">Bookshelf</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="height: 73px; background-color: #fff">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Bookshelf</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                  <a class="nav-link navbar-links " href="../index.html" style=" margin-right: 31px;">Home</a>
+                    <a class="nav-link navbar-links" href="../index.html">Home</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link navbar-links" href="../favoritos/index.html" style=" margin-right: 31px;">Favoritos</a>
+                    <a class="nav-link navbar-links" href="../favoritos/index.html">Favoritos</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link navbar-links" href="../lidos/lidos.html" style="">Lidos</a>
+                    <a class="nav-link navbar-links" href="../lidos/lidos.html">Lidos</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../login/login.html" style="display: flex; justify-content: center; align-items: center; color: #fff; background-color: #337ab7; width: 120px; height: 35px; border-radius: 8px; padding: 6px 12px; font-family: 'Nunito', sans-serif;">Entrar</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link navbar-links" href="../login/login.html" style=" margin-left: 456px; ">Entrar</a>
+                    <a class="nav-link" href="../cadastro/cadastro.html" style="display: flex; justify-content: center; align-items: center; color: #fff; margin-left: 26px; background-color: #337ab7; width: 120px; height: 35px; border-radius: 8px; padding: 6px 12px; font-family: 'Nunito', sans-serif;">Cadastre-se</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="../cadastro/cadastro.html" style="display: flex; justify-content: center; align-items: center; color: #fff; margin-left: 26px; background-color: #337ab7; width: 120px; height: 35px; border-radius: 8px; padding: 6px 12px; font-family: 'Nunito', sans-serif;">Cadastre-se</a>
-              </li>
-              </ul>
-            </div>
-          </div>
-      </nav>`;
+            </ul>
+        </div>
+    </div>
+</nav>`;
 
   } else {
 
@@ -73,31 +75,34 @@ const initLogin = function () {
       let dados =
         document.getElementById('navbarLoggedIn').innerHTML = ` 
               
-              <nav class="navbar navbar-expand-lg bg-body-tertiary" style=" height: 73px; background-color: #fff">
-              <div class="container-fluid" style="padding-left: 160px; ">
-                <a class="navbar-brand" href="#">Bookshelf</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav" style="display: flex; align-items: center;">
-                <li class="nav-item">
-                  <a class="nav-link navbar-links" aria-current="page" href="../index.html" style=" margin-right: 31px;">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link navbar-links" href="../favoritos/index.html" style=" margin-right: 31px;">Favoritos</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link navbar-links" href="../lidos/lidos.html" style="">Lidos</a>
-                </li>
-  
-                <li class="nav-item" style="display: flex; align-items: center;">
-
-                </li>
-              </ul>
-                </div>
-              </div>
-            </nav>`;
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" style="height: 73px; background-color: #fff">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Bookshelf</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link navbar-links" aria-current="page" href="../index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-links" href="../favoritos/index.html">Favoritos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link navbar-links" href="../lidos/lidos.html">Lidos</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto" style="display: flex; align-items: center;">
+                    <li class="nav-item">
+                        <a href="../perfil/PaginaPerfil.html" style="text-decoration: none"> <img src="${usuario.picture}" id="imgUser" style="border-radius: 30px; width:50px;"></a>
+                        <span id="loginUser" style="margin-left: 10px;"> ${usuario.usuario}</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    `;
 
     }
   }
--- pesquisados/script.js
+++ pesquisados/script.js
@@ -74,33 +74,35 @@ const mostraLivro = function(data){
           
           // Mostra a navbar para quando o usuário não está logado
           document.getElementById('navbarLoggedOut').innerHTML = `
-              <nav class="navbar navbar-expand-lg bg-body-tertiary" style=" height: 73px; background-color: #fff">
-              <div class="container-fluid" style="padding-left: 160px; ">
-                <a class="navbar-brand" href="#">Bookshelf</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <nav class="navbar navbar-expand-lg bg-body-tertiary" style="height: 73px; background-color: #fff">
+          <div class="container-fluid">
+              <a class="navbar-brand" href="#">Bookshelf</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav">
-                    <li class="nav-item">
-                      <a class="nav-link navbar-links " href="../index.html" style=" margin-right: 31px;">Home</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link navbar-links" href="../favoritos/index.html" style=" margin-right: 31px;">Favoritos</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link navbar-links" href="../lidos/lidos.html" style="">Lidos</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link navbar-links" href="../login/login.html" style=" margin-left: 456px; ">Entrar</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="../cadastro/cadastro.html" style="display: flex; justify-content: center; align-items: center; color: #fff; margin-left: 26px; background-color: #337ab7; width: 120px; height: 35px; border-radius: 8px; padding: 6px 12px; font-family: 'Nunito', sans-serif;">Cadastre-se</a>
-                  </li>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav me-auto">
+                      <li class="nav-item">
+                          <a class="nav-link navbar-links" href="../index.html">Home</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link navbar-links" href="../favoritos/index.html">Favoritos</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link navbar-links" href="../lidos/lidos.html">Lidos</a>
+                      </li>
                   </ul>
-                </div>
+                  <ul class="navbar-nav ms-auto">
+                      <li class="nav-item">
+                          <a class="nav-link" href="../login/login.html" style="display: flex; justify-content: center; align-items: center; color: #fff; background-color: #337ab7; width: 120px; height: 35px; border-radius: 8px; padding: 6px 12px; font-family: 'Nunito', sans-serif;">Entrar</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="../cadastro/cadastro.html" style="display: flex; justify-content: center; align-items: center; color: #fff; margin-left: 26px; background-color: #337ab7; width: 120px; height: 35px; border-radius: 8px; padding: 6px 12px; font-family: 'Nunito', sans-serif;">Cadastre-se</a>
+                      </li>
+                  </ul>
               </div>
-          </nav>`;
+          </div>
+      </nav>`;
       
         } else {
       
@@ -111,32 +113,33 @@ const mostraLivro = function(data){
             let dados =
               document.getElementById('navbarLoggedIn').innerHTML = ` 
                   
-                  <nav class="navbar navbar-expand-lg bg-body-tertiary" style=" height: 73px; background-color: #fff">
-                  <div class="container-fluid" style="padding-left: 160px; ">
-                    <a class="navbar-brand" href="#">Bookshelf</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <nav class="navbar navbar-expand-lg bg-body-tertiary" style="height: 73px; background-color: #fff">
+              <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Bookshelf</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav" style="display: flex; align-items: center;">
-                    <li class="nav-item">
-                      <a class="nav-link navbar-links" aria-current="page" href="../index.html" style=" margin-right: 31px;">Home</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link navbar-links" href="../favoritos/index.html" style=" margin-right: 31px;">Favoritos</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link navbar-links" href="../lidos/lidos.html" style="">Lidos</a>
-                    </li>
-      
-                    <li class="nav-item" style="display: flex; align-items: center;">
-                    <a href="../perfil/PaginaPerfil.html" style="text-decoration: none"> <img src="${usuario.picture}" id="imgUser" style="border-radius: 30px;width:50px;margin-left: 450px; "></img></a>
-                    <span id="loginUser" style="margin-left: 10px;"> ${usuario.usuario}</span>
-                    </li>
-                  </ul>
-                    </div>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                      <ul class="navbar-nav me-auto">
+                          <li class="nav-item">
+                              <a class="nav-link navbar-links" aria-current="page" href="../index.html">Home</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link navbar-links" href="../favoritos/index.html">Favoritos</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link navbar-links" href="../lidos/lidos.html">Lidos</a>
+                          </li>
+                      </ul>
+                      <ul class="navbar-nav ms-auto" style="display: flex; align-items: center;">
+                          <li class="nav-item">
+                              <a href="../perfil/PaginaPerfil.html" style="text-decoration: none"> <img src="${usuario.picture}" id="imgUser" style="border-radius: 30px; width:50px;"></a>
+                              <span id="loginUser" style="margin-left: 10px;"> ${usuario.usuario}</span>
+                          </li>
+                      </ul>
                   </div>
-                </nav>`;
+              </div>
+          </nav>`;
       
           }
         }
